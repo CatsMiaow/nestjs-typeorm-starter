da4bc9e3309cae870d0f1a4f8098628008c0988d
CatsMiaow <infiltrator@naver.com>
2020-08-28 13:17:17 +0900

    Fix config

--- src/config/development.ts
+++ src/config/development.ts
@@ -3,22 +3,11 @@ export const config = {
     type: 'mysql',
     synchronize: true,
     logging: true,
-    replication: {
-      master: {
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'username',
-        password: 'password',
-        database: 'dbname',
-      },
-      slaves: [{
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'username',
-        password: 'password',
-        database: 'dbname',
-      }],
-    },
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'username',
+    password: 'password',
+    database: 'dbname',
     extra: {
       connectionLimit: 10,
     },
